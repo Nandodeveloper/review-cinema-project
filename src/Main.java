b454ffb5e0390e8fcea380bc256b18e21abb36e9
Nandodeveloper <fernandinhomoraesbr@gmail.com>
2025-02-24 22:55:31 -0300

    implements arrayList

--- src/Main.java
+++ src/Main.java
@@ -2,57 +2,77 @@ import br.com.alura.screenmatch.calculator.TimeCalculator;
 import br.com.alura.screenmatch.models.Episode;
 import br.com.alura.screenmatch.models.Movie;
 import br.com.alura.screenmatch.models.Series;
+import br.com.alura.screenmatch.models.Title;
 import br.com.alura.screenmatch.rating.RecommendationFilter;
+
+import java.util.ArrayList;
 
 
 public class Main {
     public static void main(String[] args) {
-        Movie movie1 = new Movie();
-        movie1.setName("The GoodFather");
-        movie1.setDirector("Francis Ford");
-        movie1.setDuration(180);
-        movie1.setReleaseYear(1972);
-        movie1.setIncludedInPlan(true);
-        movie1.rate(8.0);
-        movie1.rate(7.0);
-        movie1.rate(5.0);
-        movie1.rate(9.0);
-        movie1.getClassification();
-        //movie1.displayTitle();
+        Movie goodFather = new Movie();
+        goodFather.setName("The GoodFather");
+        goodFather.setDirector("Francis Ford");
+        goodFather.setDuration(180);
+        goodFather.setReleaseYear(1972);
+        goodFather.rate(8.5);
+        goodFather.getClassification();
 
-        Movie movie2 = new Movie();
-        movie2.setDuration(150);
+        Movie avatar = new Movie();
+        avatar.setName("Avatar");
+        avatar.setReleaseYear(2009);
+        avatar.setDirector("James Cameron");
+        avatar.setDuration(162);
+        avatar.rate(8);
+        avatar.getClassification();
+
+        Movie joker = new Movie();
+        joker.setName("Joker");
+        joker.setReleaseYear(2019);
+        joker.setDirector("Todd Phillips");
+        joker.setDuration(122);
+        joker.rate(9);
+        joker.getClassification();
 
         Series dexter = new Series();
         dexter.setName("Dexter");
         dexter.setReleaseYear(1972);
-        dexter.setIncludedInPlan(true);
         dexter.setMinutesPerEp(54);
         dexter.setSeasons(8);
         dexter.setEpPerSeason(12);
         dexter.setActive(false);
-        dexter.rate(8.0);
-        dexter.rate(7.0);
-        dexter.rate(5.0);
-        dexter.rate(9.0);
-        dexter.displayTitle();
-        System.out.println("Duration to marathon dexter: "+dexter.getDuration()+" min");
+        dexter.rate(8.5);
+        dexter.getClassification();
 
         TimeCalculator timeCalculator = new TimeCalculator();
-        timeCalculator.includes(movie1);
-        timeCalculator.includes(movie2);
+        timeCalculator.includes(goodFather);
+        timeCalculator.includes(avatar);
         timeCalculator.includes(dexter);
-        System.out.println(timeCalculator.getFullTime());
+        timeCalculator.includes(joker);
+        //System.out.println(timeCalculator.getFullTime());
 
         RecommendationFilter filter = new RecommendationFilter();
-        filter.filter(movie1);
-        filter.filter(movie2);
-        filter.filter(dexter);
+//        filter.filter(goodFather);
+//        filter.filter(avatar);
+//        filter.filter(dexter);
+//        filter.filter(joker);
 
         Episode ep = new Episode();
         ep.setNumber(1);
         ep.setSeries(dexter);
         ep.setTotalViews(300);
-        filter.filter(ep);
+        //filter.filter(ep);
+
+        ArrayList<Movie> movieArray = new ArrayList<>();
+        movieArray.add(goodFather);
+        movieArray.add(avatar);
+        movieArray.add(joker);
+//        System.out.println(movieArray.size());
+//        System.out.println(movieArray.get(0).getName());
+        for (int i = 0; i < movieArray.size(); i++) {
+            System.out.println("Movie: " + movieArray.get(i).getName()
+                    + " (" + movieArray.get(i).getReleaseYear() + ")");
+        }
+        System.out.println(movieArray.toString());
     }
 }
--- src/br/com/alura/screenmatch/models/Title.java
+++ src/br/com/alura/screenmatch/models/Title.java
@@ -69,4 +69,9 @@ public class Title {
             System.out.println("Total ratings: " + totalRatings);
         }
     }
+
+    @Override
+    public String toString() {
+        return "Movie: " + this.getName() + " (" + this.getReleaseYear() + ")";
+    }
 }
